e048f0936f86b1f5d197ac46892b0b4c6be68006
Luke Groeninger <luke@dghost.net>
2015-02-19 04:36:48 -0700

    Move game.h into q2vr repo (shared directory)

--- g_local.h
+++ g_local.h
@@ -7,7 +7,7 @@
 // short, server-visible gclient_t and edict_t structures,
 // because we define the full size ones in this file
 #define	GAME_INCLUDE
-#include "game.h"
+#include "../qcommon/shared/game.h"
 #include "player/p_menu.h"
 #include "player/p_text.h"
 #include "km_cvar.h"
