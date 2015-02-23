990dd8ddc206432aaffa612e79bcac8f6f190345
Luke Groeninger <luke@dghost.net>
2015-02-23 02:40:18 -0700

    Update memory tag definition to match q2vr engine

--- g_local.h
+++ g_local.h
@@ -104,9 +104,8 @@
 #define	FRAMETIME		0.1
 
 // memory tags to allow dynamic memory to be cleaned up
-#define	TAG_GAME	765		// clear when unloading the dll
-#define	TAG_LEVEL	766		// clear when loading a new level
-
+#define	TAG_GAME	0x7FFD		// clear when unloading the dll
+#define	TAG_LEVEL	0x7FFE		// clear when loading a new level
 
 #define MELEE_DISTANCE	80
 
